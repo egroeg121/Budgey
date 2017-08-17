ed573588dfd9a0a4a0e57ca3e5d2c34c3e08ddff
George Barnet <george.barnett.121@gmail.com>
2017-08-17 15:10:13 +0100

    1
    Deleted everything

--- app/src/main/java/barnett/george/budgey/MainActivity.java
+++ app/src/main/java/barnett/george/budgey/MainActivity.java
@@ -2,58 +2,20 @@ package barnett.george.budgey;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    Button RecurringButton;
-    Button CategoriesPageButton;
-    Button BudgetsPageButton;
-    Button TransactionsPageButton;
-
-
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initisalise Buttons
-        RecurringButton = (Button) findViewById(R.id.RecurringButton);
-        CategoriesPageButton = (Button) findViewById(R.id.CategoriesPageButton);
-        BudgetsPageButton = (Button) findViewById(R.id.BudgetsPageButton);
-        TransactionsPageButton = (Button) findViewById(R.id.TransactionsPageButton);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        CheckDates checkdates = new CheckDates(this);
-        checkdates.CheckRecurringDates();
-    }
-
-    public void TransactionsPageButtonClicked(View view){
-        Intent intent = new Intent(MainActivity.this, TransactionsPage.class);
-        startActivity(intent);
-    }
-
-    public void CategoriesPageButtonClicked(View view){
-        Intent intent = new Intent(MainActivity.this, CategoriesPage.class);
-        startActivity(intent);
-    }
-
-    public void RecurringButtonClicked(View view) {
-        Intent intent = new Intent(MainActivity.this, RecurringPage.class);
-        startActivity(intent);
-    }
-
-    public void BudgetsPageButtonClicked(View view){
-        Intent intent = new Intent(MainActivity.this, Budgets.class);
-        startActivity(intent);
-    }
 
 }
